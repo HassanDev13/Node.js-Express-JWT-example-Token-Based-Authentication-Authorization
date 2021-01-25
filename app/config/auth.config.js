9b749ad1c4a18884ce198bb002730491c9beb625
HassanDev13 <hassantupe1999@gmail.com>
2021-01-25 10:42:01 +0000

    practice with mySQL db rest api auth

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "<B#LCK$^(T^fuaQemeR&6s:#@AAwe@?T,c'=+kxT?euCP27R/D=uRm893$=^_h^f={c.)MD#[%zg}$K8_D#D-_/tb2?Q>RFr(}H:Fp#{&yNFt@2Y<K/GB28unz"
+  };
---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "zerrhasstlemcen9991",
+    DB: "hassdb",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
